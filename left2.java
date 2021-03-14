ebb534dfd6939abf722dbf674d3db0a8d207d47e
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-13 23:56:03 -0600

    left2

---
+++ left2.java
@@ -0,0 +1,14 @@
+// Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. 
+// The string length will be at least 2.
+
+
+// left2("Hello") → "lloHe"
+// left2("java") → "vaja"
+// left2("Hi") → "Hi"
+
+
+public String left2(String str) {
+  String firstTwo = str.substring(0,2);
+  String remaining = str.substring(2);
+  return remaining + firstTwo;
+}
